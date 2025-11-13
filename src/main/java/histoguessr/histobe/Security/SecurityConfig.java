8bab319c432fb4bc7bbeb25d1e054059353d4469
nnikolajj <greger.nikolaj@gmail.com>
2025-11-13 15:37:44 +0100

    adds Dockerfile

--- src/main/java/histoguessr/histobe/Security/SecurityConfig.java
+++ src/main/java/histoguessr/histobe/Security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://histoguessrfe.onrender.com/");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
